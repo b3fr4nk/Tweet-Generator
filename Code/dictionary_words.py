377e4ef35becb506dad997c37984dfec19df5f5b
Chris Wood <wchristopherwood912@gmail.com>
2023-01-14 09:59:14 -0800

    add autocomplete stretch challenge

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -4,10 +4,14 @@ import argparse
 def get_words_list():
     file = open("/usr/share/dict/words", 'r')
     all_words = file.readlines()
+    output = []
 
     file.close()
+    
+    for word in all_words:
+        output.append(word.rstrip("\n"))
 
-    return all_words
+    return output
 
 def get_words(num_words):
     words = random.choices(get_words_list(), k=num_words)
@@ -24,14 +28,28 @@ def make_sentence(words):
 
     return output
 
+def auto_complete(typed_letters):
+    words = get_words_list()
+    output = []
+
+    for word in words:
+        if typed_letters == word[:len(typed_letters)]:
+            output.append(word)
+
+    return output 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('num_words', help="enter the number of words to receive", type=int)
+    # parser.add_argument('num_words', help="enter the number of words to receive", type=int)
+    parser.add_argument('letters', type=str, help="enter the start of a word to be autocompleted")
 
     args = parser.parse_args()
-    num_words = args.num_words
+    # num_words = args.num_words
+    letters = args.letters
 
-    words = get_words(num_words)
-    sentence = make_sentence(words)
+    # words = get_words(num_words)
+    # sentence = make_sentence(words)
 
-    print(sentence)
+    # print(sentence)
+    completed = auto_complete(letters)
+    print(completed)
