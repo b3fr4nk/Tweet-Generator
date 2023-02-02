a52596eff95cc1537d6a25668a15e895c997d060
Chris Wood <wchristopherwood912@gmail.com>
2023-02-01 18:58:28 -0800

    refactor

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -17,19 +17,43 @@ class Dictogram(dict):
         if word_list is not None:
             for word in word_list:
                 self.add_count(word)
+                self.tokens += 1
+
+        self.types = len(self.keys())
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        if word in self:
+            self[word] += count
+        else:
+            self[word] = count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        if word in self:
+            return self[word]
+        return 0
 
-    def sample(self):
+    def sample(self, num_words=1):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
+        keys = list(self.keys())
+        values = []
+        weight = 0
+        for word in keys:
+            weight += self[word]
+            values.append(weight)
+
+        choices = random.choices(keys, cum_weights=values, k=num_words)
+
+        text = ""
+        for choice in choices:
+            text += f"{choice}"
+
+        return text
 
 
 def print_histogram(word_list):
