87976f6a1993e43953ece769d95fcc6e569747ea
Chris Wood <wchristopherwood912@gmail.com>
2023-02-01 21:52:26 -0800

    removed old code

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,48 +1,5 @@
 import random
 
-# def histogram(word_list):
-#     histogram = {}
-
-#     #adds the word if it is already in the histogram, otherwise increments by 1
-#     for word in word_list:
-#         if word in histogram:
-#             histogram[word] += 1
-#         else:
-#             histogram[word] = 1
-    
-#     return histogram
-
-# def unique_words(histogram):
-#     return len(histogram.keys())
-
-# def frequency(histogram, word):
-#     return histogram[word]
-
-# def get_words(file_path):
-#     file = open(file_path, "r")
-#     word_list = []
-
-#     words = file.readline()
-#     while words != "":
-#         words = words.rstrip('\n')
-#         word_list.extend(words.split(" "))
-
-#         words = file.readline()
-
-#     return word_list
-
-# def sample(histogram):
-#     keys = list(histogram.keys())
-#     values = []
-#     weight = 0
-#     for word in keys:
-#         weight += histogram[word]
-#         values.append(weight)
-
-#     choice = random.choices(keys, cum_weights=values)
-
-#     return choice[0]
-
 class Histogram():
     def __init__(self, in_file):
         self.file = in_file
